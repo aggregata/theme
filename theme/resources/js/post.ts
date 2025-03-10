25cdfab2518a65898d384308b910eecf44e3ed41
Jairus Joer <jairus.joer@hero-software.de>
2025-03-10 19:39:06 +0100

    Update redirects and externalize links

--- theme/resources/js/post.ts
+++ theme/resources/js/post.ts
@@ -11,3 +11,10 @@ setTimeout(async () => {
     const { default: renderMathInText } = await import('katex/contrib/auto-render/auto-render.js');
     renderMathInText(richText);
 }, 0);
+
+
+// Externalize all link references within the rich text
+setTimeout(() => {
+    const links = richText?.querySelectorAll('a');
+    links?.forEach((link) => link.setAttribute('target', '_blank'));
+}, 0);
